c914d4548b1bf30f7294c492db50e26d00f022d4
Brian Su <brian18088@gmail.com>
2022-10-18 17:05:25 +0800

    Create (12) Integer to Roman.cpp

---
+++ cpp/(12) Integer to Roman.cpp
@@ -0,0 +1,20 @@
+// Source : https://leetcode.com/problems/integer-to-roman/submissions/
+// Author : Brian SU
+// Date   : 2022-10-18
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<string> sym({"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"});
+        vector<int> number({1000,900,500,400,100,90,50,40,10,9,5,4,1});
+        string a;
+        
+        for(int i=0;i<number.size();i++){
+            if (num >= number[i]){
+                for(int j=0;j<(num/number[i]);j++){
+                    a=a+sym[i];}
+                num = num % number[i];}
+        }
+        return a;
+    }
+}; 
