78471048ebc439730af75d80edc152ca7c34f108
Brian Su <brian18088@gmail.com>
2022-10-18 16:04:43 +0800

    Create (7) Reverse Integer.cpp

---
+++ cpp/(7) Reverse Integer.cpp
@@ -0,0 +1,20 @@
+// Source : https://leetcode.com/problems/reverse-integer/
+// Author : Brian SU
+// Date   : 2022-10-18
+
+class Solution {
+public:
+    int reverse(int x) {
+        int y=0;
+        while(x!=0){
+            if(y>INT_MAX/10 || y<INT_MIN/10){
+                return 0;
+            }
+            else{
+                y=y*10+x%10;
+                x=x/10;
+            }
+        }
+        return y;
+    }
+};
