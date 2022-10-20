a27417eea1039f61341c8850e57eff324ac47ef2
Brian Su <brian18088@gmail.com>
2022-10-20 23:39:27 +0800

    Create (17) Letter Combinations of a Phone Number.cpp

---
+++ cpp/(17) Letter Combinations of a Phone Number.cpp
@@ -0,0 +1,47 @@
+// Source : https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+// Author : Brian SU
+// Date   : 2022-10-20
+
+https://assets.leetcode.com/uploads/2022/03/15/1200px-telephone-keypad2svg.png
+
+class Solution {
+    private:
+    string tb[10]={
+        " ", //0
+        " ", //1
+        "abc", //2
+        "def", //3
+        "ghi", //4
+        "jkl", //5 
+        "mno", //6
+        "pqrs", //7
+        "tuv", //8
+        "wxyz" //9
+    };
+    vector<string> result;
+    
+    void trans(int idx, string& digits, string& r){
+        if(idx==digits.size()){
+            result.push_back(r);
+            return;
+        }
+        else{
+            int num = digits[idx]-'0';
+            for(int i=0;i<tb[num].size();i++){
+                r[idx] = tb[num][i];
+                trans(idx+1,digits,r);
+            }        
+        }
+    }
+    
+    public:
+    vector<string> letterCombinations(string digits) {
+        int len=digits.size();
+        result.clear();
+        if(len==0){
+            return result;}
+        string r(digits.size(),' ');
+        trans(0,digits,r);
+        return result;
+    }
+};
