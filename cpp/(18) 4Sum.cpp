09f9d824ff814858afc29660f6e13b95244d311d
Brian Su <brian18088@gmail.com>
2022-10-22 00:03:08 +0800

    Create (18) 4Sum.cpp

---
+++ cpp/(18) 4Sum.cpp
@@ -0,0 +1,35 @@
+// Source : https://leetcode.com/problems/4sum/
+// Author : Brian SU
+// Date   : 2022-10-22
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, long target) {
+        
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                int k = j+1;
+                int m = nums.size()-1;
+                long a = target-nums[i]-nums[j];
+                while(k < m){
+                    if(nums[k] + nums[m] == a){
+                        vector <int> v = {nums[i], nums[j], nums[k], nums[m]};
+                        if(find(ans.begin(),ans.end(),v) == ans.end()){
+                             ans.push_back(v);
+                            }
+                        k++;
+                        m--;
+                    }
+                    else if(nums[k] + nums[m] > a){
+                        m--;}
+                    else{
+                        k++;}
+                    }
+            }
+        }
+        return ans;
+    }
+};
