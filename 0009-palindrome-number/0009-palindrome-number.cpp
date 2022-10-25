5906f94e621d8832507e67f4f01a0bc528b8a4a9
Brian Su <brian18088@gmail.com>
2022-10-26 00:46:18 +0800

    Time: 32 ms (37.91%), Space: 5.7 MB (90.79%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long a=x;
+        
+        if(x<0 || (x!=0 && x%10==0) ){
+            return false;}
+        
+        long num=0;
+        
+        while(x>0){
+            num=num*10+x%10;
+            x=x/10;}
+        if(num==a){
+            return true;}
+        
+     return false;
+    }
+};
