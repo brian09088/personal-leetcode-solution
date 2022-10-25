8f7148655f57ff0d69a6cefc61de16627b7a3a37
Brian Su <brian18088@gmail.com>
2022-10-26 00:44:48 +0800

    Time: 3 ms (68.56%), Space: 5.7 MB (95.35%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int y=0;
+        while(x!=0){
+            if(y>INT_MAX/10 || y<INT_MIN/10){
+                return 0;
+            }
+            else{
+                y=y*10+x%10;
+                x=x/10;
+            }
+        }
+        return y;
+    }
+};
