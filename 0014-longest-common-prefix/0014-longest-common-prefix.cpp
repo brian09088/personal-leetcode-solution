2aa5b8405fe6075e0da9494b143eb965b6561e0b
Brian Su <brian18088@gmail.com>
2022-10-26 00:53:35 +0800

    Time: 17 ms (11.58%), Space: 15.3 MB (6.43%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans;
+        char c;
+        if(strs.size() < 1){
+            return ans;
+        }
+        else if(strs.size() == 1){
+            return strs[0];
+        }
+        for(int i=0;i<strs[0].size();i++){
+            c=strs[0][i];
+            for(auto s : strs){
+                if(i+1 > s.size() || c!=s[i]){
+                    return ans;
+                }
+            }
+            ans.push_back(c);
+        }
+        return ans;
+    }
+};
