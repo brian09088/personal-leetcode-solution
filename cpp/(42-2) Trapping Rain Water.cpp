e9f2f97124d9f906ce39aeafc5c9f49168194d92
Brian SU <brian18088@gmail.com>
2022-12-16 15:51:24 +0800

    Update (42-2) Trapping Rain Water.cpp

--- cpp/(42-2) Trapping Rain Water.cpp
+++ cpp/(42-2) Trapping Rain Water.cpp
@@ -1,1 +1,20 @@
+# stack structure
 
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        stack<int> st;
+        int res=0,i=0,n=height.size();
+        while(i < n){
+            if(st.empty() || height[i] <= height[st.top()]){
+                st.push(i++);}
+            else{
+                int t = st.top(); st.pop();
+                if(st.empty()){
+                    continue;}
+                res += (min(height[i],height[st.top()]) - height[t]) * (i-st.top()-1); 
+            }
+        }
+        return res;
+    }
+};
