98753197374ddd71e3e6bac4d9a294c6040b63c7
Brian SU <brian18088@gmail.com>
2022-12-16 14:09:47 +0800

    Create (42) Trapping Rain Water.cpp

---
+++ cpp/(42) Trapping Rain Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, left = 0, right = height.size()-1, n = height.size();
+        while(left < right){
+            int minimum = min(height[left] , height[right]);
+            if(minimum == height[left]){
+                left++;
+                while(left < right && height[left] < minimum){
+                    res += minimum - height[left++];
+                }
+            }
+            else{
+                right--;
+                while(left < right && height[right] < minimum){
+                    res += minimum - height[right--];
+                }
+            }
+        }
+        return res;
+    }
+};
