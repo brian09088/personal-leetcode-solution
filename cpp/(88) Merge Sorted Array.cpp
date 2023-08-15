efd5ddaf02372b7a42df3e756b7067429e311d79
Brian Su <brian18088@gmail.com>
2023-08-15 16:33:09 +0800

    Create (88) Merge Sorted Array.cpp

---
+++ cpp/(88) Merge Sorted Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for (int i = 0 , j = m ; i < n ; i++){
+            nums1[j] = nums2[i];
+            j++;
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+};
