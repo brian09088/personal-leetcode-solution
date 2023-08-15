9c7573e69c5b8ced5199b27278e653c52ac8bfed
Brian Su <brian18088@gmail.com>
2023-08-15 11:20:38 +0800

    Create (74) Search a 2D Matrix.cpp

---
+++ cpp/(74) Search a 2D Matrix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if(matrix.empty() || matrix[0].empty()){return false;}
+        int row = 0;
+        int col = matrix[0].size()-1;
+        while(row < matrix.size() && col >= 0){
+            if(matrix[row][col] == target){return true;}
+            else if(matrix[row][col] > target){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+};
